b9cfabf1e8ed9edc222d57449f5b46e4dd900a52
Your Name <your_email@example.com>
2023-08-05 13:06:55 +0530

    Add Basic Layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Box } from "@mui/material";
+import { Footer, Navbar } from "./Components/index";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
 
 const App = () => {
-  return <div>Hi Ankit</div>;
+  return (
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
