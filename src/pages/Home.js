34fc2b6164b8321d02ddd52884f0801d7702dd99
Your Name <your_email@example.com>
2023-08-05 16:18:41 +0530

    Add BodyPart and Scrollbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
 import { Exercises, HeroBanner, SearchExercises } from "../Components/index";
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
       <Exercises />
     </Box>
   );
