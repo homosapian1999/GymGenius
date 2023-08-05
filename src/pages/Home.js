0f1e3933772d6304dc06e3a583ac92003bf3b6e2
Your Name <your_email@example.com>
2023-08-05 14:12:48 +0530

    Add Hero Banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Box } from "@mui/material";
+import { Exercises, HeroBanner, SearchExercises } from "../Components/index";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
 };
 
 export default Home;
